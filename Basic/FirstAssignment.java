836e1221b9af1ac3d67f9c0b97e792ec81107074
vikram <vikramchoudhary1116@gmail.com>
2024-12-09 01:06:31 +0530

    First Assignment Uploaded on GitHub

---
+++ Basic/FirstAssignment.java
@@ -0,0 +1,34 @@
+public class FirstAssignment{
+    public static void main(String[] args){
+        //Assignment 1st : 7//12/24
+
+        //Q.1 hello world
+        System.out.println("Hello! World");
+
+        //Q.2 five friends 
+        System.out.println("Five Friends: ");
+        System.out.println("1) Roland Kunert");
+        System.out.println("2) Naveen");
+        System.out.println("3) Nikhil");
+        System.out.println("4) Ronak");
+        System.out.println("5) Virendra");
+
+        //Q.3 five fav food name :new line 
+        System.out.println("Five Fav Food : ");
+        System.out.println("1) Dal Bati");
+        System.out.println("2) Panner");
+        System.out.println("3) Roll");
+        System.out.println("4) Burger");
+        System.out.println("5) Pizza");
+
+
+    //Q.4 five city name :new line 
+    System.out.println("Five City : ");
+    System.out.println("1) Tokyo");
+    System.out.println("2) Osaka");
+    System.out.println("3) Delhi");
+    System.out.println("4) Jaipur");
+    System.out.println("5) Channai");
+
+    }
+}
