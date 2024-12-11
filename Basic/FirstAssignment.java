69a29cde379d9c951e284054051c90b4a5867c48
vikram <vikram.choudhary1116@gmail.com>
2024-12-11 18:46:19 +0530

    Second Assignment Uploaded on GitHub

--- Basic/FirstAssignment.java
+++ Basic/FirstAssignment.java
@@ -1,11 +1,11 @@
 public class FirstAssignment{
     public static void main(String[] args){
-        //Assignment 1st : 7//12/24
+//Assignment 1st : 7//12/24
 
-        //Q.1 hello world
+    //Q.1 hello world
         System.out.println("Hello! World");
 
-        //Q.2 five friends 
+    //Q.2 five friends 
         System.out.println("Five Friends: ");
         System.out.println("1) Roland Kunert");
         System.out.println("2) Naveen");
@@ -13,7 +13,7 @@ public class FirstAssignment{
         System.out.println("4) Ronak");
         System.out.println("5) Virendra");
 
-        //Q.3 five fav food name :new line 
+    //Q.3 five fav food name :new line 
         System.out.println("Five Fav Food : ");
         System.out.println("1) Dal Bati");
         System.out.println("2) Panner");
