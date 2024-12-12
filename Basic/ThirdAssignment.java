f8862054dbdabec466244c4e7d178b0b12aea5a6
vikram <vikram.choudhary1116@gmail.com>
2024-12-12 11:43:37 +0530

    Third Assignment Uploaded on GitHub

---
+++ Basic/ThirdAssignment.java
@@ -0,0 +1,155 @@
+import java.util.*;
+public class ThirdAssignment{
+    public static void main(String [] args){
+    //Assignment 2nd : 9//12/24
+
+      //Q.1 47+6
+        int a = 47;
+        int b = 6;
+        int c = a+b;
+        System.out.println("Ans :-> Sum : " + c);
+    
+      //Q.2 234-55
+
+        int d = 234;
+        int e = 55;
+        int f = d-e;
+        System.out.println("Ans :-> Substration : " + f);
+
+      //Q.3 56*4
+
+        int g = 56;
+        int h = 4;
+        int i = g*h;
+        System.out.println("Ans :-> Multiplication : " + i);
+
+      //Q.4 22/2
+
+        int j = 22;
+        int k = 2;
+        int l = j/k;
+        System.out.println("Ans :-> Division : " + l);
+
+      //Q.5 45+67
+
+        int m = 45;
+        int n = 67;
+        int o = m+n;
+        System.out.println("Ans :-> Sum : " + o);
+
+      //Q.6 23-2
+
+        int p = 23;
+        int q = 2;
+        int r = p-q;
+        System.out.println("Ans :-> Sub : " + r);
+
+      //Q.7 34*5
+
+        int s = 34;
+        int t = 5;
+        int v = s*t;
+        System.out.println("Ans :-> multi : " + v);
+
+      //Q.8 34+67+23+12+6 
+
+        int w = 34;
+        int x = 67;
+        int y = 23;
+        int z = 12;
+        int aa = 6;
+        int sum1 = w+x+y+z+aa;
+        System.out.println("Ans :-> Sum : " + sum1);
+
+      //Q.9 23+89+12+56
+
+        int bb = 23;
+        int cc = 89;
+        int dd = 12;
+        int ee = 12;
+        int ff = 56;
+        int gg = bb+cc+dd+ee+ff;
+        System.out.println("Ans :-> Sum : " + gg);
+
+      //Q10 56*34+12/2-13 
+
+        float hh = 56f;
+        float ii = 34f;
+        float jj = 12f;
+        float kk = 2f;
+        float ll = 12f;
+        float mm = (hh*ii) + (jj / (kk - ll));
+        System.out.println("Ans :-> Total : " + mm);
+
+      //Q11 1.2*6.7-4.5/2 
+         
+         float nn = 1.2f;
+         float pp = 6.7f;
+         float qq = 4.5f;
+         float rr = 2f;
+         float ss = (nn*pp)-(qq/rr);
+         System.out.println("Ans :-> Result : " + ss);
+
+      //Q12 1.6*(12-2)/2+7.8-1.5  
+
+          float tt = 1.6f;
+          float uu = 12f;
+          float vv = 2f;
+          float ww = 7.8f;
+          float xx = 1.5f;
+          float yy = (tt*(uu-vv))/(vv+ww-xx);
+          System.out.println("Ans :-> Result : " + yy);
+
+      //Q13 4.5*1.2-(82/2)-5.6 +1.234 
+
+           float zz = 4.5f;
+           float num = 1.2f;
+           float num1 = 82f;
+           float num2 = 2f;
+           float num3 = 2f;
+           float num4 = 5.6f;
+           float num5 = 1.234f;
+           float total = (zz*num) - (num1/num2) - (num4 + num5);  
+           System.out.println("Ans :-> Result : " + total);
+
+      //Q14 8*1+(4-2)/1+6
+
+            float num6 = 8f;
+            float num7 = 1f;
+            float num8 = 4f;
+            float num9 = 2f;
+            float num10 = 1f;
+            float num11 = 6f;
+            float num12 = (num6*num7) + (num8-num9)/(num10 + num11);
+            System.out.println("Ans :-> Result : " + num12);
+
+
+      //Q15 3.4+1.2*(3.7-1.2)/2+6.8
+
+            float num13 = 3.4f;
+            float num14 = 1.2f;
+            float num15 = 3.7f;
+            float num16 = 1.2f;
+            float num17 = 2f;
+            float num18 = 6.8f;
+            float total1 = (num13 + num14) * (num15-num16)/(num17+num18);
+            System.out.println("Ans :-> Result : " + total1);
+
+      //Q16 8.9*1.67-(6.21-5.6)+7.8-34 +157
+           
+             float num19 = 8.9f;
+             float num20 = 1.67f;
+             float num21 = 6.21f;
+             float num22 = 5.6f;
+             float num23 = 7.8f;
+             float num24 = 34f;
+             float num25 = 157f;
+             float total2 = (num19*num20) - (num21 - num22) + num23 - num24 + num25;   
+             System.out.println("Ans :-> Result : " + total2);
+
+
+
+        // Part 2 Continues
+
+    }
+}
