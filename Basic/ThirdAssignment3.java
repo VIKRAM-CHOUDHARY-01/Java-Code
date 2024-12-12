ab5c06ca388c06b19fa494e1b249ea160b1ac447
vikram <vikram.choudhary1116@gmail.com>
2024-12-12 21:56:12 +0530

    Third Assignment Part 3 Uploaded on GitHub

---
+++ Basic/ThirdAssignment3.java
@@ -0,0 +1,227 @@
+import java.util.*;
+public class ThirdAssignment3{
+    public static void main(String [] args){
+     //Assignment 3rd : 9//12/24 -> Part 3
+
+        //Q1 45+7 
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Number : ");
+        // int a = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int b = sc.nextInt();
+        
+        // int sum = a+b;
+        // System.out.println("Total : " + sum);
+
+
+        //Q2 234-55
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Number : ");
+        // int c = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int d = sc.nextInt();
+        
+        // int sub = c-d;
+        // System.out.println("Total : " + sub);
+
+
+        //q.3 56*4
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter Number : ");
+        // int e = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int f = sc.nextInt();
+        // int g = e*f;
+        // System.out.println("Total : " + g);
+
+
+        //Q.4 22/2
+
+        // System.out.println("Enter Number : ");
+        // int h = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int i = sc.nextInt();
+        // int j = h/i;
+        // System.out.println("Total : " + j);
+
+        //Q.5 45+67
+
+        // System.out.println("Enter Number : ");
+        // int h = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int i = sc.nextInt();
+        // int j = h+i;
+        // System.out.println("Total : " + j);
+
+        //Q.6 23-2 
+
+
+        // System.out.println("Enter Number : ");
+        // int h = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int i = sc.nextInt();
+        // int j = h-i;
+        // System.out.println("Total : " + j);
+
+
+        //Q.7 34*5
+
+        // System.out.println("Enter Number : ");
+        // int h = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int i = sc.nextInt();
+        // int j = h*i;
+        // System.out.println("Total : " + j);
+
+
+        //Q.8 34+67+23+12+6 
+
+        // System.out.println("Enter Number : ");
+        // int h = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int i = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int j = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int k = sc.nextInt();
+        // System.out.println("Enter Number : ");
+        // int l = sc.nextInt();
+        // int m = h+i+j+k+l;
+        // System.out.println("Total : " + m);
+
+
+        //Q.9 23+89+12+56
+
+        System.out.println("Enter Number : ");
+        int h = sc.nextInt();
+        System.out.println("Enter Number : ");
+        int i = sc.nextInt();
+        System.out.println("Enter Number : ");
+        int j = sc.nextInt();
+        System.out.println("Enter Number : ");
+        int k = sc.nextInt();
+        int m = h+i+j+k;
+        System.out.println("Total : " + m);
+
+        //Q10 56*34+12/2-13 
+
+        // System.out.println("Enter Number : ");
+        // float h = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float i = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float j = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float k = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float l = sc.nextFloat();
+        // float m = (h*i) +(j/(k-l));
+        // System.out.println("Total : " + m);
+
+        //Q11 1.2*6.7-4.5/2 
+
+        // System.out.println("Enter Number : ");
+        // float h = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float i = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float j = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float k = sc.nextFloat();
+        // float m = (h*i) - (j/k);
+        // System.out.println("Total : " + m);
+
+        //Q12 1.6*(12-2)/2+7.8-1.5  
+
+        // System.out.println("Enter Number : ");
+        // float h = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float i = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float j = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float k = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float l = sc.nextFloat();
+        // float m = h*((i-j) / (j+k-l));
+        // System.out.println("Total : " + m);
+
+        //Q13 4.5*1.2-(82/2)-5.6 +1.234 
+        
+        System.out.println("Enter Number : ");
+        float h = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float i = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float j = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float k = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float l = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float n = sc.nextFloat();
+        float m = (h*i) - (j/k) - l + n;
+        System.out.println("Total : " + m);
+
+        //Q14 8*1+(4-2)/1+6
+
+        // System.out.println("Enter Number : ");
+        // float h = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float i = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float j = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float k = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float l = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float n = sc.nextFloat();
+        // float m = h*i+((j-k)/(l+n));
+        // System.out.println("Total : " + m);
+
+        //Q15 : 3.4+1.2*(3.7-1.2)/2+6.8
+
+        // System.out.println("Enter Number : ");
+        // float h = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float i = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float j = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float k = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float l = sc.nextFloat();
+        // System.out.println("Enter Number : ");
+        // float n = sc.nextFloat();
+        // float m = (h+i)*((j-k)/(l+n));
+        // System.out.println("Total : " + m);
+
+        //Q16 8.9*1.67-(6.21-5.6)+7.8-34 +157
+
+        System.out.println("Enter Number : ");
+        float h = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float i = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float j = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float k = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float l = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float n = sc.nextFloat();
+        System.out.println("Enter Number : ");
+        float p = sc.nextFloat();
+        float m = (h*i) - (j-k) + l-n+p;
+        System.out.println("Total : " + m);
+
+
+        // End Assignment
+
+
+    }
+}
