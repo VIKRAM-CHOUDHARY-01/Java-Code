dd8e855459abe4a640fb3c250cb76e78f79dcdd4
vikram <vikramchoudhary1116@gmail.com>
2024-12-24 23:23:21 +0530

    Constructor in java

---
+++ OPPS/Encapsulation/Constructor.java
@@ -0,0 +1,13 @@
+class Con{
+    Con(){
+        System.out.println("Hello from Constructor");
+    }
+
+}
+public class Constuctor{
+    public static void main(String[]args){
+
+        
+
+    }
+}
