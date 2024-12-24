e2139c4bd55e0f428b7738aa4c2d1b7a1db248bf
vikram <vikram.choudhary1116@gmail.com>
2024-12-24 23:26:02 +0530

    Constructor in java

--- OPPS/Encapsulation/Constructor.java
+++ OPPS/Encapsulation/Constructor.java
@@ -1,13 +1,21 @@
-class Con{
-    Con(){
-        System.out.println("Hello from Constructor");
+class Con {
+    // Public default constructor
+    public Con() {
+        System.out.println("Hello from Public Default Constructor");
     }
 
+    // Public parameterized constructor
+    public Con(String message) {
+        System.out.println("Hello from Public Parameterized Constructor: " + message);
+    }
 }
-public class Constuctor{
-    public static void main(String[]args){
 
-        
+public class ConstructorExample {
+    public static void main(String[] args) {
+        // Creating an object using the public default constructor
+        Con obj1 = new Con();
 
+        // Creating an object using the public parameterized constructor
+        Con obj2 = new Con("Welcome to Java!");
     }
 }
