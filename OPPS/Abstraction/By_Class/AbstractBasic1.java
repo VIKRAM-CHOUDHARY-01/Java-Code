95356d6b7654e23626d91ad69301ad56af0ad3c7
vikram <vikram.choudhary1116@gmail.com>
2025-01-11 12:11:42 +0530

    Abstract Basic

---
+++ OPPS/Abstraction/By_Class/AbstractBasic1.java
@@ -0,0 +1,11 @@
+abstract class A{
+    //Abstract method : always declare
+    // absract method does not define in abstract class
+
+    abstract void show();
+}
+public class AbstractBasic1{
+    public static void main(String [] args){
+
+    }
+}
