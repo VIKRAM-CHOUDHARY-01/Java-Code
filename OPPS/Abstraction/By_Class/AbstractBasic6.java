ff1411e533e6d8a45a36dbecca1c74d052901e37
vikram <vikram.choudhary1116@gmail.com>
2025-01-11 12:16:28 +0530

    Abstract Basic 6

---
+++ OPPS/Abstraction/By_Class/AbstractBasic6.java
@@ -0,0 +1,23 @@
+abstract class A{
+    abstract void show();
+    void display(){ // non abstract method is always declared and defined in abstract class
+        System.out.println("Nice");
+    }
+}
+class B extends A{
+    void show(){
+        System.out.println("Hello!");
+    }
+}
+public class AbstractBasic6{
+    public static void main(String [] args){
+
+        B ob= new B();
+        ob.display() ;
+
+        A obj = new B(); // through reference variable
+        obj.display();
+      
+        
+    }
+}
