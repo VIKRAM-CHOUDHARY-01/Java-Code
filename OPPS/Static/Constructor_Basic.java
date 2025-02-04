c479a0631937885d806da2a469a661769853521d
vikram <vikram.choudhary1116@gmail.com>
2025-02-05 00:40:46 +0530

    Public Constructor

---
+++ OPPS/Static/Constructor_Basic.java
@@ -0,0 +1,11 @@
+class A{
+    public A(){
+        System.out.println("Public constructor");
+    }
+}
+public class Constructor_Basic{
+    public static void main(String [] args){
+        new A();
+
+    }
+}
