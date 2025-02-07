f3d4a36add9a250ead4cb9e0403eeb90f1e3d55a
vikram <vikram.choudhary1116@gmail.com>
2025-02-07 19:08:53 +0530

    Interface Concept in Aggregation

---
+++ OPPS/Aggregation/Interface_in_Aggregation/MainAggregation.java
@@ -0,0 +1,9 @@
+public class MainAggregation implements A{
+    public void show(){
+        System.out.println("Hello!");
+    }
+    public static void main(String [] args){
+        MainAggregation ma =  new MainAggregation();
+        ma.show();
+    }
+}
