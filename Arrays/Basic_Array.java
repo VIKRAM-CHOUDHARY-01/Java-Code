eb009d4d50ad3b238221252aae2daa59faf41e48
vikram <vikram.choudhary1116@gmail.com>
2025-02-12 00:01:27 +0530

    Basic Array Concept

---
+++ Arrays/Basic_Array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Basic_Array{
+    public static void main(String [] args){
+        int [] a = {44,6,868,47,8};
+        for(int i =0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+        System.out.println("-----------------------------------------------------------");
+        // int b[] = new int[5];
+        // Scanner sc= new Scanner(System.in);
+        // for(int i =0;i<b.length;i++){
+        //     System.out.println("Enter Number : ");
+        //     b[i] = sc.nextInt();
+        // }
+        // for(int i=0;i<b.length;i++){
+        //     System.out.println(b[i]);
+        // }
+         System.out.println("-----------------------------------------------------------");
+
+        // to find the length of array :
+        System.out.println(a.length);
+
+    }
+}
