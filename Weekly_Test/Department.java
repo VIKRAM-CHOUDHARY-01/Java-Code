d0e9d65f3a766343217c39c7e086803c846751d8
vikram <vikram.choudhary1116@gmail.com>
2025-03-06 23:03:35 +0530

    Weekly test Question

---
+++ Weekly_Test/Department.java
@@ -0,0 +1,11 @@
+
+/*
+Q8
+Create a Department class that contains an object of an Employee class to show aggregation.
+*/
+class Department{
+    public static void main(String [] args){
+        Employee obj = new Employee();
+        obj.show();
+    }
+}
