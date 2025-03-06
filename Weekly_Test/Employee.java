01aa283a0e2a0456869c8b24d7da2cbd6fe34eec
vikram <vikram.choudhary1116@gmail.com>
2025-03-06 23:04:03 +0530

    Weekly test Question

---
+++ Weekly_Test/Employee.java
@@ -0,0 +1,5 @@
+class Employee{
+    void show(){
+        System.out.println("Hello My name is Vikram Choudhary and I am an employess of I don't know Maybe I am not Right but you got the idea");
+    }
+} 
